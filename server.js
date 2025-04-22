76ae9831b446217d889861a97c50e571ebc3ce05
Siddharam <siddharamsutar23@gmail.com>
2025-04-22 07:34:14 +0530

    A

--- server.js
+++ server.js
@@ -6,13 +6,9 @@ import resisterrouter from './routes/resister.js';
 import busrouter from './routes/bus.js'
 import ticketrouter from './routes/ticketroute.js'
 import busModel from './models/bus.js';
-import stripe from 'stripe';
 dotenv.config()
 const app=express();
-app.use(cors({
-    origin:"*",
-    methods:['GET','POST','PUT','DELETE']
-}));
+app.use(cors());
 app.use(express.json());
 Connect(process.env.MONGO).then(()=>{
     console.log('connected db');
@@ -21,32 +17,6 @@ Connect(process.env.MONGO).then(()=>{
 app.use('/me',resisterrouter);
 app.use('/bus',busrouter);
 app.use('/ticket',ticketrouter);
-
-app.post('/pay', async (req, res) => {
-    try {
-        const { amount } = req.body; 
-
-        if (!amount || amount <= 0) {
-            return res.status(400).json({ message: 'Invalid amount' });
-        }
-
-        // Convert INR to paise (Stripe requires the smallest currency unit)
-        const totalAmount = amount * 100;
-
-        // Create a payment intent
-        const paymentIntent = await stripe.paymentIntents.create({
-            amount: totalAmount,
-            currency: 'inr',
-        });
-
-        res.status(200).json({
-            message: 'Payment initiated',
-            clientSecret: paymentIntent.client_secret,
-        });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
 
 app.post('/addbusces',async(req,res)=>{
     try {
